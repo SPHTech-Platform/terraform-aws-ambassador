26386efb0deb97dc5ba4d18cf4bc5de2d6b56115
Poh Peng Ric Wang <7760361+thepoppingone@users.noreply.github.com>
2022-09-22 16:30:40 +0800

    update required provider version

--- modules/dns/versions.tf
+++ modules/dns/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28"
+      version = ">= 4.10.0"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 }
